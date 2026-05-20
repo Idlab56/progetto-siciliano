2567cfd2020bb623c45c9d87897cd95f603b38ad
Riccardo <baldi.riccardo@informatica.lcl>
2026-05-20 11:34:06 +0200

    Aggiungi HttpClientModule

--- progetto/src/app/app.component.ts
+++ progetto/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { Data } from './service/data';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet, ReactiveFormsModule, FormsModule],
+  imports: [IonApp, IonRouterOutlet, ReactiveFormsModule, FormsModule, HttpClientModule],
 })
 export class AppComponent {
   constructor(private dataService: Data) {}
