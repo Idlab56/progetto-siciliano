590c977b54eb4fc31d658bc014f2507eee6a3de2
Riccardo <baldi.riccardo@informatica.lcl>
2026-05-20 10:43:41 +0200

    guazzo nero1

--- progetto/src/app/app.routes.ts
+++ progetto/src/app/app.routes.ts
@@ -10,4 +10,16 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'dettaglio',
+    loadComponent: () => import('./src/app/dettaglio/dettaglio.page').then( m => m.DettaglioPage)
+  },
+  {
+    path: 'profilo',
+    loadComponent: () => import('./src/app/profilo/profilo.page').then( m => m.ProfiloPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./src/app/login/login.page').then( m => m.LoginPage)
+  },
 ];
